266dfd4b5706b499a5bb4d617797418a5dc658cd
yeonwoo <chiyy6162@gmail.com>
2022-04-28 16:41:20 +0900

    [feat] footer 컴포넌트 생성 및 연결

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <Logo />
+    <a href="https://github.com/yeonwoochoe" target="_blank">
+      (c){{ new Date().getFullYear() }} YEONWOO
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: 0.3;
+  a {
+    display: block;
+    margin-bottom: 4px;
+    text-decoration: none;
+  }
+}
+</style>
