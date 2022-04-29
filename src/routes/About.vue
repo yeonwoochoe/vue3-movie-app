b7e2ad7b85c56217e04e09cc0b2df14183fb0fe5
yeonwoo <chiyy6162@gmail.com>
2022-04-29 18:56:19 +0900

    [feat] components : about 구성 및 연결

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,3 +1,87 @@
 <template>
-  <h1>about!</h1>
+  <div class="about">
+    <div class="photo">
+      <Loader v-if="imageLoading" absolute />
+      <img :src="image" :alt="name" />
+    </div>
+    <div class="name">{{ name }}</div>
+    <div>{{ email }}</div>
+    <div>{{ blog }}</div>
+    <div>{{ phone }}</div>
+  </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+import Loader from "~/components/Loader.vue";
+export default {
+  components: { Loader },
+  data() {
+    return {
+      imageLoading: true,
+    };
+  },
+  computed: {
+    //모듈명시와, 배열데이터 추가하여 내가 가져올 상태(데이터)명을 표시하여 이 내용들이 computed로 객체데이터로 반환이 되어
+    //그 객체데이터를  전개연산자로 등록하여 쓰는 것!
+    //직접 mapState 를 할당하는 것보다는 전개연산자를 통해 computed 객체데이터 내부에 쓰는것을 추천.
+    ...mapState("about", ["image", "name", "email", "blog", "phone"]),
+
+    // image() {
+    //   return this.$store.state.about.image;
+    // },
+    // name() {
+    //   return this.$store.state.about.name;
+    // },
+    // email() {
+    //   return this.$store.state.about.email;
+    // },
+    // blog() {
+    //   return this.$store.state.about.blog;
+    // },
+    // phone() {
+    //   return this.$store.state.about.phone;
+    // },
+  },
+  // 라이프사이클에선 비동기를 사용할 수 없기때문에
+  //별도의 메소드 옵션을 사용하여 비동기식 로직을 사용하고 그로직을
+  //불러와서 실행하는것으로 쓸 수 있음
+  mounted() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      await this.$loadImage(this.image);
+      this.imageLoading = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.about {
+  text-align: center;
+  .photo {
+    position: relative;
+    width: 250px;
+    height: 250px;
+    margin: 40px auto 20px;
+    padding: 30px;
+    border: 10px solid $gray-300;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background-color: $gray-200;
+    img {
+      width: 100%;
+    }
+  }
+  .name {
+    font-size: 40px;
+    font-family: "Oswald", sans-serif;
+    margin-bottom: 20px;
+  }
+  div {
+    padding: 3px 0;
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,12 @@
+export default {
+  namespaced: true,
+  //하나의 데이터, 함수로 만드는 이유는 객체데이터는 배열데이터와 동일하게 참조형 데이터이기때문에
+  //데이터의 불변성을 유지해주기 위해 state 내부에 안에 데이터가 고유해질 수있도록 해주는것
+  state: () => ({
+    name: "Choe Yeon Woo",
+    email: "chiyy6162@gmail.com",
+    blog: "개편중",
+    phone: "+82-10-9999-9999",
+    image: "/static/my-emotion.png",
+  }),
+};
